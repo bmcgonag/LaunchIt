a85e7bf3d95d8788541dcec88116f82de6d5c3cf
bmcgonag <bmcgonag@gmail.com>
2015-01-11 21:47:44 -0600

    Added the view of the icon selected, and cleaned up the path settings, as well as add some ocmments.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,27 +20,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_btnAddaction_clicked()
 {
-    if (ui->txtActionName->text().isEmpty() || ui->txtActionExec->text().isEmpty()) {
-        if (ui->txtActionName->text().isEmpty()) {
-            ui->txtActionName->setStyleSheet("border: 2px solid red");
-        }
-
-        if (ui->txtActionExec->text().isEmpty()) {
-            ui->txtActionExec->setStyleSheet("border: 2px solid red");
-        }
-
-        QMessageBox msgBox;
-        msgBox.setText("You are missing required information. Please correct the fields outlined in red.");
-        msgBox.exec();
-    } else {
-        ui->listActions->addItem(ui->txtActionName->text() + " : " + ui->txtActionExec->text());
-        ui->txtActionName->setText("");
-        ui->txtActionExec->setText("");
-    }
+    // add the extra Action item name and command to the list-view
+    ui->listActions->addItem(ui->txtActionName->text() + " : " + ui->txtActionExec->text());
+    ui->txtActionName->setText("");
+    ui->txtActionExec->setText("");
 }
 
 void MainWindow::on_btnRemoveAction_clicked()
 {
+    // remove the selected item from the extra Actions list view
     QListWidgetItem *itemRemove = ui->listActions->currentItem();
     delete itemRemove;
     ui->btnRemoveAction->setEnabled(false);
@@ -48,13 +36,18 @@ void MainWindow::on_btnRemoveAction_clicked()
 
 void MainWindow::on_btnFindImage_clicked()
 {
-    QStringList filepath = QFileDialog::getOpenFileNames(this, tr("Open File"), "~/");
+    // open the system file dialog to find the icon image desired
+
+    QDir userHome = QDir::home().absolutePath();
+    QString userHomePath = userHome.absolutePath();
+    QStringList filepath = QFileDialog::getOpenFileNames(this, tr("Open File"), userHomePath);
     QString filepathstring = filepath.first();
     ui->txtImagePath->setText(filepathstring);
 }
 
 void MainWindow::on_btnCreateLauncher_clicked()
 {
+    // set the value based on the checkbox selection for Terminal
     QString terminalValue;
     if (ui->cbTerminal->isChecked()) {
         terminalValue = "True";
@@ -62,8 +55,12 @@ void MainWindow::on_btnCreateLauncher_clicked()
         terminalValue = "False";
     }
 
-
+        // set the path for the icon folder
         QDir homepath = QDir::home().absolutePath().append("/.local/share/applications/" + ui->txtIconName->text());
+
+        // if the folder already exists warn the user (if part)
+        // if not, then create the folder, and generate the .desktop file (else part)
+
         if (QDir(homepath).exists()) {
             QMessageBox message;
             message.setText("Folder Exists - Please choose a different Application Name to avoid conflicts with existing apps.");
@@ -105,12 +102,18 @@ void MainWindow::on_txtExecCmd_textChanged(const QString &arg1)
 
 void MainWindow::on_txtImagePath_textChanged(const QString &arg1)
 {
+    // make sure all required fields are filled in
     EnableDisableCreateLauncher();
+
+    // show a scaled version of the selected image on the Window
+    ui->IconImage->setScaledContents(true);
+    ui->IconImage->setPixmap(QPixmap(ui->txtImagePath->text()));
 }
 
-//    enable the Remove Action button
+
 void MainWindow::on_listActions_itemClicked(QListWidgetItem *item)
 {
+    // make sure an item is selected in the listbox, then enable the Remove Action button
     if (item->isSelected()) {
         ui->btnRemoveAction->setEnabled(true);
     }
@@ -127,6 +130,8 @@ void MainWindow::on_txtActionExec_textChanged(const QString &arg1)
 }
 
 void MainWindow::EnableDisableCreateLauncher() {
+
+    // ensure that all required fields for creating a launcher are filled then enable the button
     if (!ui->txtIconName->text().isEmpty() && !ui->txtExecCmd->text().isEmpty()
             && !ui->txtImagePath->text().isEmpty()) {
         ui->btnCreateLauncher->setEnabled(true);
@@ -136,6 +141,8 @@ void MainWindow::EnableDisableCreateLauncher() {
 }
 
 void MainWindow::EnableDisableAddAction() {
+
+    // ensure that all required fields for Adding an extra Action are filled then enable the button
     if (!ui->txtActionExec->text().isEmpty() && !ui->txtActionName->text().isEmpty()) {
         ui->btnAddaction->setEnabled(true);
     } else {
@@ -144,6 +151,10 @@ void MainWindow::EnableDisableAddAction() {
 }
 
 void MainWindow::BlankFields() {
+
+    // blank out all fields after a successful .desktop creation
+    // there's probably a more elegant way to do this.
+
     ui->txtActionExec->setText("");
     ui->txtActionName->setText("");
     ui->txtComment->setText("");
